3153a44879b94cdeb7887fe1f17e76f0d5442f22
nahid <nahidkahn9144@gmail.com>
2024-02-07 00:04:54 +0530

    use of props and usestate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
+import Form from './components/Form'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nahid khan
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar/>
+   <Form heading = "hello nahid"/>
+  {/* <div className="container"> */}
+   {/* <div className="heart"> */}
+  {/* <Form/> */}
+   {/* </div> */}
+  {/* </div> */}
+   </>
   );
 }
 
