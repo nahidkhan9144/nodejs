3c84ef274098f1f82c5d201a87fe2acc3cd94913
nahid <nahidkahn9144@gmail.com>
2024-08-07 21:33:47 +0530

    changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import {ACCESS_TOKEN_NAME} from '../constants/apiConstants';
 import { useNavigate } from 'react-router-dom';
 import useCheckToken  from './useCheckToken';
-export default function Home() {
+import Navbar from './Navbar';
+import Form from './Form';
+export default function Home(props) {
   let navigate = useNavigate();
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const [buttonPath, setButtonPath] = useState('/card');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert('Dark mode enabled', 'success');
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode enabled', 'success');
+    }
+  };
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+
+  const handleButtonClick = () => {
+    setButtonPath('/card');
+  };
   // debugger;
   useCheckToken();
   const handleLogout = () => {
@@ -17,9 +47,10 @@ export default function Home() {
   
   return (
     <>
-    <div>Home</div>
-    <button type="submit" className="btn btn-warning" onClick={handleLogout}>Logout</button>
-    <button type="submit" className="btn btn-warning" onClick={newsbtn}>News</button>
+    <Navbar title="textutils" alert={alert} mode={mode} showAlert={showAlert} buttonClickHandler={handleButtonClick} toggleMode={toggleMode} />
+    <button type="submit" className="btn btn-warning mx-3 my-3 mb-3" onClick={handleLogout}>Logout</button>
+    <button type="submit" className="btn btn-warning mx-3 my-3 mb-3" onClick={newsbtn}>GO To News</button>
+    <Form heading="hello nahid" buttonClickHandler={handleButtonClick}  mode={mode} showAlert={showAlert} toggleMode={toggleMode} />
  </>
   )
 }
